b5806f3e5eee9f4e227302b65c1c2e2754b114c8
Captain Shadows <captain_shadows@aol.com>
2013-08-31 18:56:23 -0400

    stuff

---
+++ common/ccm/compresstion/utils/lib/Archive.java
@@ -0,0 +1,19 @@
+/**
+ * CCM Modding, Compresstion
+ */
+package ccm.compresstion.utils.lib;
+
+/**
+ * Archive
+ * <p>
+ * 
+ * @author Captain_Shadows
+ */
+public class Archive
+{
+    public static final String MOD_ID = "compresstion";
+
+    public static final String MOD_NAME = "Compresstion";
+
+    public static final String MOD_PREFIX = "compresstion";
+}
