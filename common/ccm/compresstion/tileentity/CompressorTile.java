11b1f3f564325e90047b53821f30864a488779ef
Captain Shadows <captain_shadows@aol.com>
2013-11-28 15:00:50 -0500

    Removed the not needed param

--- common/ccm/compresstion/tileentity/CompressorTile.java
+++ common/ccm/compresstion/tileentity/CompressorTile.java
@@ -161,7 +161,7 @@ public class CompressorTile extends ProgressTE implements ISidedInventory
             } else
             // if it is not
             {
-                ItemStack tmp = new ItemStack(ModBlocks.compressedBlock, 1);
+                ItemStack tmp = new ItemStack(ModBlocks.compressedBlock);
                 NBTHelper.setInteger(tmp, Archive.NBT_COMPRESSED_BLOCK_ID, block.blockID);
                 NBTHelper.setByte(tmp, Archive.NBT_COMPRESSED_BLOCK_META, meta);
                 stack = tmp;
