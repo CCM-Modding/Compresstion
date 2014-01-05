9ee9eb7d28c3e29dcafefccdbf513fc2090b192c
captain_shadows <captain_shadows@aol.com>
2014-01-05 04:14:27 -0500

    Lets see If THAT will make it work....
    Signed-off-by: captain_shadows <captain_shadows@aol.com>

--- common/ccm/compression/Compression.java
+++ common/ccm/compression/Compression.java
@@ -20,7 +20,7 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.GameRegistry;
 
-@Mod(modid = MOD_ID, useMetadata = true)
+@Mod(modid = MOD_ID, dependencies = "required-after:nucleum_omnium", useMetadata = true)
 @NetworkMod(clientSideRequired = true, serverSideRequired = false)
 public class Compression extends CCMMod
 {
