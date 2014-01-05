dbc6dd8d71fe70bad3cd1f928082f8efb3b69442
Captain Shadows <captain_shadows@aol.com>
2014-01-05 04:10:50 -0500

    Create Properties.java
    It won't let me add it ....

---
+++ common/ccm/compression/utils/lib/Properties.java
@@ -0,0 +1,9 @@
+package ccm.compression.utils.lib;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Properties
+{
+    public static List<Integer> blackList = new ArrayList<Integer>();
+}
